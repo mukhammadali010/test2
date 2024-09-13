fe7c8fb0e764d6c956202ecd6a0487bb223084e5
Mukhammadali <108802022+mukhammadali010@users.noreply.github.com>
2024-09-13 21:30:14 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let ism = prompt('ism kiriting...');
+let harf = prompt("Harf kiriting");
+
+if(ism.includes(harf)){
+    alert(`ha , ${harf} harfi qatnashgan`)
+}else{
+    alert('yoq bunday harf qatnashmagan')
+}
+
